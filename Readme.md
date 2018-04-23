e0140cf03abe2ebd44cc24f195702912327ca7be
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to export a map to a file
+
+
+To export a map to a file use one of the following methods.<br />- <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ExportToHtmltopic">MapControl.ExportToHtml</a> - exports a map to a Html file<br />- <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ExportToImagetopic">MapControl.ExportToImage</a> - exports a map as an image.<br />- <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ExportToMhttopic">MapControl.ExportToMht</a> - exports a map to a Mht file.<br />- <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ExportToPdftopic">MapControl.ExportToPdf</a> - exports a map to a Pdf file.<br />- <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ExportToRtftopic">MapControl.ExportToRtf</a> - exports a map to a Rtf file.<br />- <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ExportToXlstopic">MapControl.ExportToXls</a> - exports a map to a Xls file.<br />- <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ExportToXlsxtopic">MapControl.ExportToXlsx</a> - exports a map to a Xlsx file.<br />- <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ExportToXpstopic">MapControl.ExportToXps</a> - exports a map to a Xps file.
+
+<br/>
+
+
---
+++ VB/Exporting/MainWindow.xaml.vb
@@ -0,0 +1,51 @@
+﻿Imports System
+Imports System.Windows
+Imports DevExpress.XtraPrinting
+
+Namespace Exporting
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Private Const filename As String = "exportedMap"
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim format As ExportFormat = CType(cbExportFormat.SelectedItem, ExportFormat)
+            Dim filepath As String
+            If format = ExportFormat.Image Then
+                filepath = String.Format("{0}.jpg", filename)
+            Else
+                filepath = String.Format("{0}.{1}", filename, format)
+            End If
+            Dim isExported As Boolean = True
+            Select Case format
+                Case (ExportFormat.Htm)
+                    mapControl.ExportToHtml(filepath)
+                Case (ExportFormat.Image)
+                    mapControl.ExportToImage(filepath)
+                Case (ExportFormat.Mht)
+                    mapControl.ExportToMht(filepath)
+                Case (ExportFormat.Pdf)
+                    mapControl.ExportToPdf(filepath)
+                Case (ExportFormat.Rtf)
+                    mapControl.ExportToRtf(filepath)
+                Case (ExportFormat.Xls)
+                    mapControl.ExportToXls(filepath)
+                Case (ExportFormat.Xlsx)
+                    mapControl.ExportToXlsx(filepath)
+                Case (ExportFormat.Xps)
+                    mapControl.ExportToXps(filepath)
+                Case Else
+                    isExported = False
+            End Select
+            If isExported Then
+                MessageBox.Show(String.Format("Map exported successfully."))
+            Else
+                MessageBox.Show(String.Format("Map exporting does not support the {0} file format.", format))
+            End If
+        End Sub
+    End Class
+End Namespace
